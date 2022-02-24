4492264bb1217571034a15341f7f9bc22d7bd9b0
Elliot Ng <elliotng@oursky.com>
2022-02-24 16:31:47 +0800

    Implement mint nft test case for invalid user address
    Ref oursky/likecoin-chain#46

--- x/likenft/keeper/msg_server_mint_nft_test.go
+++ x/likenft/keeper/msg_server_mint_nft_test.go
@@ -5,6 +5,7 @@ import (
 
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/golang/mock/gomock"
 	"github.com/likecoin/likechain/backport/cosmos-sdk/v0.46.0-alpha2/x/nft"
 	"github.com/likecoin/likechain/testutil/keeper"
@@ -466,6 +467,95 @@ func TestMintNFTIscnNotFound(t *testing.T) {
 	// Check output
 	require.Error(t, err)
 	require.Contains(t, err.Error(), types.ErrIscnRecordNotFound.Error())
+	require.Nil(t, res)
+
+	// Check mock was called as expected
+	ctrl.Finish()
+}
+
+func TestMintNFTInvalidUserAddress(t *testing.T) {
+	// Setup
+	ctrl := gomock.NewController(t)
+	accountKeeper := testutil.NewMockAccountKeeper(ctrl)
+	bankKeeper := testutil.NewMockBankKeeper(ctrl)
+	iscnKeeper := testutil.NewMockIscnKeeper(ctrl)
+	nftKeeper := testutil.NewMockNftKeeper(ctrl)
+	msgServer, goCtx, keeper := setupMsgServer(t, keeper.LikenftDependedKeepers{
+		AccountKeeper: accountKeeper,
+		BankKeeper:    bankKeeper,
+		IscnKeeper:    iscnKeeper,
+		NftKeeper:     nftKeeper,
+	})
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// Test Input
+	ownerAddressBytes := []byte{0, 1, 0, 1, 0, 1, 0, 1}
+	iscnId := iscntypes.NewIscnId("likecoin-chain", "abcdef", 1)
+	classId := "likenft1aabbccddeeff"
+	tokenId := "token1"
+	uri := "ipfs://a1b2c3"
+	uriHash := "a1b2c3"
+	metadata := types.JsonInput(
+		`{
+	"abc": "def",
+	"qwerty": 1234,
+	"bool": false,
+	"null": null,
+	"nested": {
+		"object": {
+			"abc": "def"
+		}
+	}
+}`)
+
+	// Mock keeper calls
+	classData := types.ClassData{
+		Metadata:     types.JsonInput(`{"aaaa": "bbbb"}`),
+		IscnIdPrefix: iscnId.Prefix.String(),
+		Config: types.ClassConfig{
+			Burnable: false,
+		},
+	}
+	classDataInAny, _ := cdctypes.NewAnyWithValue(&classData)
+	nftKeeper.
+		EXPECT().
+		GetClass(gomock.Any(), gomock.Eq(classId)).
+		Return(nft.Class{
+			Id:          classId,
+			Name:        "Class Name",
+			Symbol:      "ABC",
+			Description: "Testing Class 123",
+			Uri:         "ipfs://abcdef",
+			UriHash:     "abcdef",
+			Data:        classDataInAny,
+		}, true)
+
+	keeper.SetClassesByISCN(ctx, types.ClassesByISCN{
+		IscnIdPrefix: iscnId.Prefix.String(),
+		ClassIds:     []string{classId},
+	})
+
+	iscnKeeper.
+		EXPECT().
+		GetContentIdRecord(gomock.Any(), gomock.Eq(iscnId.Prefix)).
+		Return(&iscntypes.ContentIdRecord{
+			OwnerAddressBytes: ownerAddressBytes,
+			LatestVersion:     1,
+		})
+
+	// Run
+	res, err := msgServer.MintNFT(goCtx, &types.MsgMintNFT{
+		Creator:  "not a valid address",
+		ClassId:  classId,
+		Id:       tokenId,
+		Uri:      uri,
+		UriHash:  uriHash,
+		Metadata: metadata,
+	})
+
+	// Check output
+	require.Error(t, err)
+	require.Contains(t, err.Error(), sdkerrors.ErrInvalidAddress.Error())
 	require.Nil(t, res)
 
 	// Check mock was called as expected
