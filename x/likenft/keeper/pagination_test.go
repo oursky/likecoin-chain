d41ad9e277de98af97afcd181101a697801e17cb
Elliot Ng <elliotng@oursky.com>
2022-03-08 03:23:32 +0800

    Add pagination test for key-based normal case
    Ref oursky/likecoin-chain#76

--- x/likenft/keeper/pagination_test.go
+++ x/likenft/keeper/pagination_test.go
@@ -68,3 +68,49 @@ func TestPaginationNormalOffset(t *testing.T) {
 	}, res4)
 	require.Equal(t, []int{9}, actualPage4)
 }
+
+func TestPaginationNormalKey(t *testing.T) {
+	var actualPage1 []int
+	res1, err := keeper.PaginateArray(10, &query.PageRequest{
+		Limit: 4,
+	}, func(i int) error {
+		actualPage1 = append(actualPage1, i)
+		return nil
+	}, 5, 10)
+	require.NoError(t, err)
+	require.Equal(t, &query.PageResponse{
+		NextKey: []byte("4"),
+		Total:   uint64(10),
+	}, res1)
+	require.Equal(t, []int{0, 1, 2, 3}, actualPage1)
+
+	var actualPage2 []int
+	res2, err := keeper.PaginateArray(10, &query.PageRequest{
+		Limit: 4,
+		Key:   []byte("4"),
+	}, func(i int) error {
+		actualPage2 = append(actualPage2, i)
+		return nil
+	}, 5, 10)
+	require.NoError(t, err)
+	require.Equal(t, &query.PageResponse{
+		NextKey: []byte("8"),
+		Total:   uint64(10),
+	}, res2)
+	require.Equal(t, []int{4, 5, 6, 7}, actualPage2)
+
+	var actualPage3 []int
+	res3, err := keeper.PaginateArray(10, &query.PageRequest{
+		Limit: 4,
+		Key:   []byte("8"),
+	}, func(i int) error {
+		actualPage3 = append(actualPage3, i)
+		return nil
+	}, 5, 10)
+	require.NoError(t, err)
+	require.Equal(t, &query.PageResponse{
+		NextKey: nil,
+		Total:   uint64(10),
+	}, res3)
+	require.Equal(t, []int{8, 9}, actualPage3)
+}
